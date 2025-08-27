0077571fcc17a74ede2afde3fe1395b79cdaf0e5
Jameel Shehadeh <jamshehadeh@gmail.com>
2025-08-27 14:52:14 +0400

    Update Work Section

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -15,29 +15,56 @@ const Works = () => {
 					<div className="works-body">
 						<div className="work">
 							<img
-								src="./facebook.png"
-								alt="facebook"
+								src="./evolutionLogo.png"
+								alt="evolution services"
 								className="work-image"
 							/>
-							<div className="work-title">Facebook</div>
+							<div className="work-title">Evolution Services</div>
 							<div className="work-subtitle">
-								Software Engineer
+								Senior iOS Software Engineer
 							</div>
-							<div className="work-duration">2019 - Present</div>
+							<div className="work-duration">Oct 2023 - Present Dubai</div>
 						</div>
 
 						<div className="work">
 							<img
-								src="./twitter.png"
-								alt="twitter"
+								src="./peakPerformance.png"
+								alt="peak performance"
 								className="work-image"
 							/>
-							<div className="work-title">Twitter</div>
+							<div className="work-title">Peak Performance Technology</div>
 							<div className="work-subtitle">
-								Software Engineer
+								iOS Software Engineer
 							</div>
-							<div className="work-duration">2019 - Present</div>
+							<div className="work-duration">Sep 2022 - Oct 2023 Dubai</div>
 						</div>
+
+						<div className="work">
+							<img
+								src="./projectLogos/arabicLogo.png"
+								alt="Arabic by multilingualism"
+								className="work-image"
+							/>
+							<div className="work-title">Arabic by multilingualism</div>
+							<div className="work-subtitle">
+								iOS Developer
+							</div>
+							<div className="work-duration">Feb 2021 - May 2022 UK(Remote)</div>
+						</div>
+
+						<div className="work">
+							<img
+								src="./moeLogo.png"
+								alt="ministry of education"
+								className="work-image"
+							/>
+							<div className="work-title">Ministry of education - Dubai</div>
+							<div className="work-subtitle">
+								Programming Instructor
+							</div>
+							<div className="work-duration">May 2017 - Jan 2021</div>
+						</div>
+						
 					</div>
 				}
 			/>
