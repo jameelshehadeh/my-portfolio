ce3e63d28709aa0bd3c50d46c9e9e6e793105373
Jameel Shehadeh <jamshehadeh@gmail.com>
2025-08-28 15:17:15 +0400

    Update portfolio

--- src/data/articles.js
+++ src/data/articles.js
@@ -2,6 +2,30 @@ import React from "react";
 
 function article_1() {
 	return {
+		date: "06 Oct 2023",
+		title: "iOS Security: Building Secure iOS Apps, an intro to CryptoKit with practical examples.",
+		description:
+			"In the ever-evolving landscape of iOS app development, ensuring the security of user data is non-negotiable. Apple’s CryptoKit framework stands as a robust solution, offering developers a seamless way to implement cryptographic operations, fortifying the privacy and integrity of sensitive information. In this introduction guide, we will explore CryptoKit’s core features through practical coding examples. By the end, you’ll be able to leverage CryptoKit to enhance the security of your iOS applications.",
+		style: ``,
+		keywords: [
+			"RxSwift",
+			"iOS Security",
+			"CryptoKit",
+			"Jameel",
+			"Jameel Shehadeh",
+			"iOS Development",
+		],
+		link: "https://medium.com/@jamshehadeh/ios-security-im-building-secure-ios-apps-an-intro-to-cryptokit-with-practical-examples-6a5a146f2e42",
+		body: (
+			<React.Fragment>
+				<h1>Content of article 3</h1>
+			</React.Fragment>
+		),
+	};
+}
+
+function article_2() {
+	return {
 		date: "23 Jun 2023",
 		title: "Your Guide To a “Thread Safe” Singleton in Swift Part I.",
 		description:
@@ -40,7 +64,7 @@ function article_1() {
 	};
 }
 
-function article_2() {
+function article_3() {
 	return {
 		date: "26 Oct 2022",
 		title: ".throttle On Observable Objects in RxSwift.",
@@ -62,6 +86,9 @@ function article_2() {
 	};
 }
 
-const myArticles = [article_1, article_2];
+
+
+
+const myArticles = [article_1, article_2, article_3];
 
 export default myArticles;
